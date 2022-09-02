5aabefcd92b7e2ce89ccc0eaf0f85fc923faa6e9
turex <salvo95ct@gmail.com>
2022-09-02 09:58:07 +0200

    update

--- repair windows.bat
+++ repair windows.bat
@@ -1,7 +1,6 @@
-set /p id=Enter Windows partition letter: 
 sfc /scannow
 Dism /Online /Cleanup-Image /RestoreHealth
-DISM /Online /Cleanup-Image /RestoreHealth /Source:WIM:%id%:\sources\Install.wim:1
+DISM /Online /Cleanup-Image /RestoreHealth /Source:WIM:C:\sources\Install.wim:1
 pause
 
 
